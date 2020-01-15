615347509a213bbfeec365c8f5e293a218668f9e
Viszkok Tamás <viszkok.tamas.93@gmail.com>
2020-01-15 14:52:25 +0100

    #2 - Issue with handling samba passwords

--- DWF-server/rebuild_and_run.bat
+++ DWF-server/rebuild_and_run.bat
@@ -5,15 +5,9 @@ docker swarm leave --force
 docker stack rm dwf_stack
 docker volume rm dwf_stack_sedstorvol
 docker volume inspect dwf_stack_sedstorvol>nul 2>&1
-IF %ERRORLEVEL%==0 (
-    echo "The dwf_stack_sedstorvol exists, using it."
-) ELSE (
-    set /p SMB_USER=Enter Samba Username: 
-
-    set psCommand="powershell -Command $pword = read-host 'Enter Samba Password' -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)"
-    
-    for /f "usebackq delims=" %%p in (`%psCommand%`) do set SMB_PASSWD=%%p
-)
+set /p SMB_USER=Enter Samba Username:
+set psCommand="powershell -Command $pword = read-host 'Enter Samba Password' -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)"
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set SMB_PASSWD=%%p
 docker build -t=dwf-server .
 docker tag dwf-server viszkoktamas93/dwf-server
 docker swarm init
--- DWF-server/rebuild_run_and_init.bat
+++ DWF-server/rebuild_run_and_init.bat
@@ -1,19 +1,13 @@
 echo off
-set SMB_VOL=//1.2.3.4/my/path/to/Deep-Water
-set SMB_DOMAIN=my_domain_name
+set SMB_VOL=//10.6.12.200/common/team/columbus/projects/kutatas/Deep-Water
+set SMB_DOMAIN=SEDDOM
 docker swarm leave --force
 docker stack rm dwf_stack
 docker volume rm dwf_stack_sedstorvol
 docker volume inspect dwf_stack_sedstorvol>nul 2>&1
-IF %ERRORLEVEL%==0 (
-    echo "The dwf_stack_sedstorvol exists, using it."
-) ELSE (
-    set /p SMB_USER=Enter Samba Username: 
-
-    set psCommand="powershell -Command $pword = read-host 'Enter Samba Password' -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)"
-    
-    for /f "usebackq delims=" %%p in (`%psCommand%`) do set SMB_PASSWD=%%p
-)
+set /p SMB_USER=Enter Samba Username:
+set psCommand="powershell -Command $pword = read-host 'Enter Samba Password' -AsSecureString ; $BSTR=[System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pword); [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)"
+for /f "usebackq delims=" %%p in (`%psCommand%`) do set SMB_PASSWD=%%p
 set DWF_INIT_DB=1
 docker build -t=dwf-server .
 docker tag dwf-server viszkoktamas93/dwf-server
