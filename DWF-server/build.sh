d3e5cf8a869368e39151278879e20170668de592
Aladics Tamás <aladics@inf.u-szeged.hu>
2021-08-27 18:35:38 +0200

    Changes in scripts.

--- DWF-server/build.sh
+++ DWF-server/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+export SMB_VOL=//10.6.12.200/common/team/columbus/projects/kutatas/Deep-Water
+export SMB_DOMAIN=SEDDOM
+read -p "Enter SAMBA Username: " s_usr
+read -sp "Enter SAMBA Password: " s_pwd
+export SMB_USER=$s_usr
+export SMB_PASSWD=$s_pwd
 sudo sysctl -w vm.max_map_count=262144
 docker-compose build 
 docker swarm init
+docker stack deploy -c docker-compose.yml dwf_stack
